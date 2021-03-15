bbd5a148b6a5bb2d86c53eeaf1c17cb809dc344e
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2021-03-15 09:25:13 +0100

    PropTypes in Button component

--- src/components/reusable/Button.js
+++ src/components/reusable/Button.js
@@ -1,11 +1,21 @@
- const Button = ({color, title}) => {
-  return <button style={{ backgroundColor: color }} className="btn">{title}</button>;
+import { PropTypes } from "prop-types";
+
+const Button = ({ color, title }) => {
+  return (
+    <button style={{ backgroundColor: color }} className="btn">
+      {title}
+    </button>
+  );
 };
 
 Button.defaultProps = {
-  title: 'Add',
-  color: 'green'
-}
+  title: "Add",
+  color: "green",
+};
 
+Button.propTypes = {
+  title: PropTypes.string,
+  color: PropTypes.string,
+};
 
-export default Button
+export default Button;
