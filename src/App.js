7e9497540dd812ec69447a610f7b8532c3fbae5a
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2021-03-15 08:59:39 +0100

    defaultProps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Header } from "./components/Header";
+import Header from "./components/Header";
 
 // Function component
 const App = () => {
   return (
     <div className="container">
-      <Header message="I'm learning props in React" />
+      <Header />
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
-export const Header = (props) => {
+ const Header = (props) => {
     return (
         <header>
-            <h1>Task Tracker</h1>
-            <p>{props.message}</p>
+            <h1>{props.title}</h1>
         </header>
     )
 }
 
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+export default Header
