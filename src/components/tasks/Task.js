e599d08e2cebac60aaf10628b1ce16f622ca9aad
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2021-03-15 10:51:43 +0100

    Pass parametere to the function from child compn

--- src/components/tasks/Task.js
+++ src/components/tasks/Task.js
@@ -5,7 +5,7 @@ const Task = ({ task, onDelete }) => {
     <div className="task">
       <h3>
       {task.name}
-      <FaTimes onClick={onDelete} style={{ color: 'red', cursor: 'pointer' }} />
+      <FaTimes onClick={() => onDelete(task.id)} style={{ color: 'red', cursor: 'pointer' }} />
       </h3>
       <p>{task.day}</p>
     </div>
